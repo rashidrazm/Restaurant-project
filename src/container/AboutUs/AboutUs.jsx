08251b1f0963c898ee3243cbe160211ee484f404
mohammed rashid <razmsi28@gmail.com>
2022-07-23 22:38:14 +0530

    project started and navbar and header section completed

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id="about" >
+    AboutUs
+  </div>
+);
+
+export default AboutUs;
